98994e49117915e6d36d9f6b5bf1e90e054484cd
Marco Freemantle <marcofreemantle@gmail.com>
2024-05-01 14:49:57 +0100

    Model, view, controller set up for in-game UI

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -6,6 +6,13 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+class UAuraUserWidget;
+class UOverlayWidgetController;
+struct FWidgetControllerParams;
+class APlayerController;
+class APlayerState;
+class UAbilitySystemComponent;
+class UAttributeSet;
 /**
  * 
  */
@@ -15,6 +22,20 @@ class AURA_API AAuraHUD : public AHUD
 	GENERATED_BODY()
 
 public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
 
+	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+
+	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 	
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
 };
